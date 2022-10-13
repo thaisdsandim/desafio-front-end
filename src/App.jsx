f4fed4b9328d44582e8d7b45843d03de0f827e99
thaisdsandim <contatothais.sandim@gmail.com>
2022-10-13 08:14:13 -0400

    add components division

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,8 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import './assets/styles/app.css'
-
-/* FUNÇÃO QUE ORGANIZA OS DADOS DOS FUNCIONARIOS EM UMA TABELA */
-
-/* Abri a função com uma list e organizei os dados que ia chamar
-    da fake API dentro da tabela de acordo com as posições que
-    estavam no figma */
-function Funcionarios({list = [] }){
-
-  return <div>
-
-  <div className='table-header'>
-    <h4>Funcionários</h4>
-    <div>
-      <input placeholder='Pesquisar' className='input'/>
-      <button className='submit-lente' type="submit">
-        <i className='fa fa-search'></i>
-      </button>
-    </div>
-  </div>
-
-  <table>
-    <thead>
-      <tr>
-        <th className='foto'>FOTO</th>
-        <th>NOME</th>
-        <th>CARGO</th>
-        <th>DATA DE ADMISSÃO</th>
-        <th className='telefone'>TELEFONE</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        list.map(({foto, nome, cargo, admissao, telefone}) => {
-          return <tr key={nome}>
-            <td><img src={foto}/></td>
-            <td>{nome}</td>
-            <td>{cargo}</td>
-            <td>{admissao}</td>
-            <td>{telefone}</td>
-          </tr>
-        })
-      }
-    </tbody>
-  </table>
-  </div>
-}
+import React, { useEffect, useState } from 'react';
+import Funcionarios from './components/Funcionarios';
+import Search from './components/Search';
+import './assets/styles/app.css';
+import axios from 'axios'
 
 /* FUNÇÃO QUE GERA O APP */
 
@@ -72,6 +28,10 @@ function App() {
       <img src='src/assets/img/logo-bemobile.png'></img>
     </header>
     <main>
+      <div className='table-header'>
+        <h4>Funcionários</h4>
+        <Search />
+      </div>
       <Funcionarios list={list}/>
     </main>
   </div>
