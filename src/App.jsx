ab60c7f1d77d450a3b7bc890f9684878247109a1
thaisdsandim <contatothais.sandim@gmail.com>
2022-10-17 15:56:34 -0400

    update search import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
-import Employees from "./components/Employees";
 import Search from "./components/Search";
+import Employees from "./components/Employees";
 import axios from "axios";
 import "./assets/styles/global.css";
 
 function App() {
   const [list, setList] = useState([]);
+  const [initial, setInitial] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3000/employees").then((response) => {
       setList(response.data);
+      setInitial(response.data);
     });
   }, []);
 
@@ -18,7 +20,7 @@ function App() {
     <div>
       <Header />
       <main>
-        <Search />
+        <Search list={list} setList={setList} initial={initial} />
         <Employees list={list} />
       </main>
     </div>
