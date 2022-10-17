9e9b78bf75b4e7c40af19f90d4cbbbb0e026019b
thaisdsandim <contatothais.sandim@gmail.com>
2022-10-17 08:39:29 -0400

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/img/logo-bemobile.png"></img>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
