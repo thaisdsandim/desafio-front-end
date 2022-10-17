d8ae658990d06bbb42090868e0446269bd8eca0f
thaisdsandim <contatothais.sandim@gmail.com>
2022-10-17 08:40:22 -0400

    update search formatation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Search() {
-
-  return <div>
-    <input 
-      type='text' 
-      placeholder='Pesquisar' 
-      className='input'
-    />
-    <button className='submit-lente' type="submit">
-        <i className='fa fa-search'></i>
-    </button>
-  </div>
-
+  return (
+    <div className="table-header">
+      <h4>Funcionários</h4>
+      <input type="text" placeholder="Pesquisar" className="input" />
+      <button className="submit-lente" type="submit">
+        <i className="fa fa-search"></i>
+      </button>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
